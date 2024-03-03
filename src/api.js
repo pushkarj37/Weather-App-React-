dbe721b74aa92a048f0481baaae47ccf113c95b0
Pushkar Jha <pushkarj37@gmail.com>
2024-03-03 23:51:53 +0530

    hiding the API keys using the env file

--- src/api.js
+++ src/api.js
@@ -2,10 +2,12 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 export const geoApiCityOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'de627f4e05msh2775afa4ebe9a84p1778edjsnae5bc4e0adaa',
+		// 'X-RapidAPI-Key': 'de627f4e05msh2775afa4ebe9a84p1778edjsnae5bc4e0adaa',
+		'X-RapidAPI-Key': process.env.REACT_APP_CITY_API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 export const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const WEATHER_API_KEY = '963ee71636fa33b9955a1d3e94f4439d';
+// export const WEATHER_API_KEY = '963ee71636fa33b9955a1d3e94f4439d';
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
